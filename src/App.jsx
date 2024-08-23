5f1c9843ac6d2cae3a20f1c6876612d87b5400a1
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-08-24 03:42:04 +0530

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './Components/Footer'
+import HomeLayout from './Layouts/HomeLayout'
 function App() {
 
   return (
@@ -12,6 +13,7 @@ function App() {
       </Routes> */}
 
       {/* <Footer/> */}
+      <HomeLayout/>
 
     </>
   )
---
+++ src/Layouts/HomeLayout.jsx
@@ -0,0 +1,13 @@
+function HomeLayout(){
+
+    return (
+        <div className="min-h-[90vh]">
+            <div className="drawer absolute left-0 z-50 w-fit">
+
+            </div>
+        </div>
+    )
+
+}
+
+export default HomeLayout;
