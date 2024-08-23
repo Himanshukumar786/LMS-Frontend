ff53afdcc513e301d6dde8c253f4be59c4ce6f2c
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-08-24 03:58:44 +0530

    layout updated

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -3,7 +3,12 @@ function HomeLayout(){
     return (
         <div className="min-h-[90vh]">
             <div className="drawer absolute left-0 z-50 w-fit">
+                <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content">
+                    <label htmlFor="my-drawer" className="curson-pointer relative">
 
+                    </label>
+                </div>
             </div>
         </div>
     )
