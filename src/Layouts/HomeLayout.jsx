b1be666561826338557832026d0cf619926b53a5
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-08-24 18:24:42 +0530

    Added login signup buttons to sidebar

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,9 +1,22 @@
-import { FiMenu } from "react-icons/fi";
-import { AiFillCloseCircle } from "react-icons/ai";
+import {AiFillCloseCircle} from 'react-icons/ai';
+import {FiMenu} from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import Footer from '../Components/Footer';
+// import { logout } from '../Redux/Slices/AuthSlice';
+
 function HomeLayout( { children } ){
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    // for checking if user is logged in
+    const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+
+    // for displaying the options acc to role
+    const role = useSelector((state) => state?.auth?.role);
 
     function changeWidth(){
         const drawerSide = document.getElementsByClassName('drawer-side');
@@ -20,8 +33,16 @@ function HomeLayout( { children } ){
 
     }
 
+    function handleLogout(e) {
+        e.preventDefault();
+
+        // const res = await dispatch(logout());
+        // if(res?.payload?.success)
+        navigate("/");
+    }
+
     return (
-        <div className="min-h-[90vh]">
+        <div className="min-h-[80vh]">
             <div className="drawer absolute left-0 z-50 w-fit">
                 <input id="my-drawer" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content">
@@ -46,6 +67,11 @@ function HomeLayout( { children } ){
                         <li>
                             <Link to="/" >Home</Link>
                         </li>
+                        {isLoggedIn && role === 'ADMIN' && (
+                            <li>
+                                <Link to="/admin/dashboard"> Admin DashBoard</Link>
+                            </li>
+                        )}
                         <li>
                             <Link to="/courses" >All Course</Link>
                         </li>
@@ -55,6 +81,33 @@ function HomeLayout( { children } ){
                         <li>
                             <Link to="/about" >About U</Link>
                         </li>
+
+                        {!isLoggedIn && (
+                            <li className="absolute bottom-4 w-[90%]">
+                                <div className="w-full flex items-center justify-center">
+                                    <button className='bg-blue-500 text-white px-4 py-1 font-semibold rounded-md w-full'>
+                                        <Link to="/login">Login</Link>
+                                    </button>
+                                    <button className='bg-pink-500 text-white px-4 py-1 font-semibold rounded-md w-full'>
+                                        <Link to="/signup">Signup</Link>
+                                    </button>
+                                </div>
+                            </li>
+                        )}
+
+                        {isLoggedIn && (
+                            <li className="absolute bottom-4 w-[90%]">
+                                <div className="w-full flex items-center justify-center">
+                                    <button className='bg-blue-500 text-white px-4 py-1 font-semibold rounded-md w-full'>
+                                        <Link to="/user/profile">Profile</Link>
+                                    </button>
+                                    <button className='bg-pink-500 text-white px-4 py-1 font-semibold rounded-md w-full'>
+                                        <Link onClick={handleLogout}>Logout</Link>
+                                    </button>
+                                </div>
+                            </li>
+                        )}
+
                     </ul>
                 </div>
             </div>
@@ -63,8 +116,7 @@ function HomeLayout( { children } ){
             <Footer/>
 
         </div>
-    )
-
+    );
 }
 
 export default HomeLayout;
