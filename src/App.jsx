825146bc27f90c0b463e0d0e6e6b4b13fb9c70bb
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-09-21 19:02:35 +0530

    Added sign in functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from './Pages/HomePage';
 import AboutUs from './Pages/AboutUs';
 import Notfound from './Pages/NotFound';
 import Signup from './Pages/Signup';
+import Login  from './Pages/Login'
 function App() {
 
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path ="/" element ={<HomePage/>} ></Route>
         <Route path ="/about" element ={<AboutUs/>} ></Route>
         <Route path="/Signup" element ={<Signup/>}></Route>
+        <Route path="/login" element ={<Login/>}></Route>
         <Route path='*'element = {<Notfound/>}></Route>
       </Routes>
 
