75a423d31871eb064c9105cf253bab9971b99dda
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-10-26 01:33:22 +0530

    Added codes

--- Server/src/controllers/courseController.js
+++ Server/src/controllers/courseController.js
@@ -16,8 +16,24 @@ const getAllCourses = async function(req, res, next) {
     }
 }
 
-const getLecturesByCourseId = async function(req, res) {
+const getLecturesByCourseId = async function(req, res, next) {
+    try {
+        const { id } = req.params;
 
+        const course = await Course.findById(id);
+
+        if (!course) {
+            return next(new AppError('Course not found', 404));
+        }
+
+        res.status(200).json({
+            success: true,
+            message: 'Course lectures fetched successfully',
+            lectures: course.lectures
+        });
+    } catch (error) {
+        return next(new AppError(error.message, 500));
+    }
 }
 
 export { getAllCourses, getLecturesByCourseId };
--- Server/src/routes/courseRoutes.js
+++ Server/src/routes/courseRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { getAllCourses, getLecturesByCourseId } from '../controllers/courseController.js';
+import isLoggedIn from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
-router.get('/', getAllCourses);
-router.get('/:id', getLecturesByCourseId);
+router.route('/')
+    .get(getAllCourses);
+
+router.route('/:id')
+    .get(isLoggedIn, getLecturesByCourseId);
 
 export default router;
